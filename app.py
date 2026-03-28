c003c756a889822353365fcafdd088964dc01393
Abubakar Sadiq Muhammad <sadiq-20@graduate.utm.my>
2026-03-28 22:42:23 +0800

    changes

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 from langchain_community.vectorstores import FAISS
-from langchain_community.embeddings import OllamaEmbeddings
+#from langchain_community.embeddings import OllamaEmbeddings
+from langchain_ollama import OllamaEmbeddings
 
 from langchain_ollama import ChatOllama
 
